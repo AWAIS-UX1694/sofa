818635d6fb37558622fa7534255869e0d91c784f
Awais <awaiskali12345@gmail.com>
2025-01-13 13:29:52 +0500

    icon1

--- src/Store/component/carticon.js
+++ src/Store/component/carticon.js
@@ -5,7 +5,7 @@ const Carticon = ()=>{
     return(
         <>
         <Link to="/cart">
-        <div className=" h-16 w-16 bg-gray-100 rounded-full p-2 hover:shadow-lg cursor-pointer  md:right-16 md:bottom-10 right-1 bottom-1 fixed">
+        <div className=" h-16 w-16 bg-gray-100 rounded-full p-2 hover:shadow-lg cursor-pointer  md:right-16 md:bottom-10 right-4 bottom-4 fixed">
             <img src={cart} alt="" />
         </div>
         </Link>
